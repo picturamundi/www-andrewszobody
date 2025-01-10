1b07309b4f3df2fd5ecbe37c0c0fb8260394de15
picturamundi <mszobody@protonmail.com>
2025-01-10 13:19:59 +0100

    add index-2

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function toggleMenu() {
     var items = document.getElementById('nav-items');
     var hamburger = document.getElementById('menu-icon');
     var cross = document.getElementById('exit-icon');
+    var name = document.getElementById('site-name');
     if (items.className.includes('hidden')) {
         items.className = 'visible';
         cross.className = 'visible';
         hamburger.className = 'hidden';
+        name.className = 'hidden';
     } else {
         items.className = 'hidden';
         cross.className = 'hidden';
         hamburger.className = 'visible';
+        name.className = 'visible';
     }
 }
