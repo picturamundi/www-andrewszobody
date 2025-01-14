859bf1320af04295157eb063acb86a6ddf6cf6f5
picturamundi <mszobody@protonmail.com>
2025-01-14 22:26:35 +0100

    hide missing image icon

--- script.js
+++ script.js
@@ -67,3 +67,9 @@ function toggleFocus() {
         social.classList.add('fadeOut');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.querySelectorAll('img').forEach(function (img) {
+        img.onerror = function () { this.style.display = 'none'; };
+    })
+});
